1df3e10675e6b7d46899c64c13fed41817190c60
Sankalp9242 <sankalpsandugade@gmail.com>
2026-05-23 11:34:54 +0530

    Configured backend for render deployment

--- sb-ecom/src/main/java/com/ecommerce/project/SbEcomApplication.java
+++ sb-ecom/src/main/java/com/ecommerce/project/SbEcomApplication.java
@@ -12,9 +12,9 @@ public class SbEcomApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SbEcomApplication.class, args);
 
-		var key = Keys.secretKeyFor(io.jsonwebtoken.SignatureAlgorithm.HS256);
-		String base64Key = Base64.getEncoder().encodeToString(key.getEncoded());
-		System.out.println(base64Key);
+//		var key = Keys.secretKeyFor(io.jsonwebtoken.SignatureAlgorithm.HS256);
+//		String base64Key = Base64.getEncoder().encodeToString(key.getEncoded());
+//		System.out.println(base64Key);
 
 
 	}
